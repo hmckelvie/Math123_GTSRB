4af94194435a322d279ff25004f9c371a8530f34
Hailey McKelvie <haileymckelvie@Haileys-MacBook-Pro-2.local>
2020-12-13 16:31:48 -0500

    added the driver code

---
+++ main.py
@@ -0,0 +1,23 @@
+import preprocess
+import pca
+import gmm
+import kmeans
+import analyze_unsupervised
+import os
+
+path = os.getcwd()
+traverse(path)
+labeled_images = preprocess.load_preprocessed(path + "/Processed") 
+
+
+img_features = pca.pca_features()
+features_df = pd.DataFrame(img_features)
+labeled_features = labeled_images.join(features_df)
+
+
+gmm_labels, gmm_score = gmm.gmm(img_features)
+
+kmeans_labels, kmeans_score = kmeans.kmeans(img_features)
+
+analyze_unsupervised.analyze_labels(labeled_images, gmm_labels, kmeans_labels)
+analyze_unsupervised.graph_all_clusters()
