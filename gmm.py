645c006e8d0aee45c4ae01075b72c27d165d15cf
Hailey McKelvie <59970500+hmckelvie@users.noreply.github.com>
2020-12-21 23:40:44 -0500

    changed for number of catagories

--- gmm.py
+++ gmm.py
@@ -3,7 +3,7 @@ import os
 from sklearn.mixture import GaussianMixture
 from sklearn.metrics import silhouette_score
 
-NUM_CLUSTER = 43 #there are 43 types of signs
+NUM_CLUSTER = 7 #there are seven classes of signs
 
 '''
 gmm
